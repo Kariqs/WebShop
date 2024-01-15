b7121b7633b6768e6edc001d8dde2e3018b8f0dc
unknown <kariukibenard189@gmail.com>
2024-01-15 13:22:37 +0300

    Second commit

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -22,7 +22,27 @@ async function addCartItem(req, res, next) {
   });
 }
 
+function updateCartItem(req, res) {
+  const cart = res.locals.cart;
+
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    req.body.quantity
+  );
+  req.session.cart = cart;
+
+  res.status(201).json({
+    message: "Product updated successfully.",
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
+}
+
 module.exports = {
   addCartItem: addCartItem,
   getCart: getCart,
+  updateCartItem: updateCartItem,
 };
--- routes/cart-routes.js
+++ routes/cart-routes.js
@@ -4,5 +4,6 @@ const cartController = require("../controllers/cart-controller");
 const router = express.Router();
 router.get("/", cartController.getCart);
 router.post("/items", cartController.addCartItem);
+router.patch("/items", cartController.updateCartItem);
 
 module.exports = router;
